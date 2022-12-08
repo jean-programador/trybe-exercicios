f7886b5f493363bc270d81e120b20577d9644ac5
Jean Carlos <jncorreia44@gmail.com>
2022-12-08 03:03:41 -0300

    feat: adiciona exercicio 5

--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/bmi.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/bmi.js
@@ -1,12 +1,35 @@
 const readline = require('readline-sync');
 
-const heigth = readline.questionInt("What your weight?");
-const weight = readline.questionInt("What your height?");
+const weight = readline.questionFloat("What your weight?");
+const height = readline.questionFloat("What your height?");
 
-function bmi(heigth, weight) {
-return weight / (heigth * 2)
+function bmi(height, weight) {
+  const heigthTotal = height ** 2;
+  const bmi = weight / heigthTotal;
+  return classificBmi(bmi.toFixed(2));
 }
 
-const result = bmi(heigth,weight)
+function classificBmi(bmi) {
+  if ( bmi < 18.5) {
+    return `${bmi}: Abaixo do peso(magreza)`
+  }
+  if (bmi >= 18.5 && bmi <= 24.9) {
+    return `${bmi}: Peso Normal`
+  }
+  if (bmi >= 25 && bmi <= 29.9) {
+    return `${bmi}: Acima do peso(sobrepeso)` 
+  }
+  if(bmi >= 30 && bmi <= 34.9) {
+    return `${bmi}: Obesidade grau I`
+  }
+  if (bmi >= 35 && bmi <= 39.9) {
+    return `${bmi}: Obesidade grau II`;
+  }
+  if (bmi > 40) {
+    return `${bmi}: Obesidade graus III e IV`;
+  }
+}
+
+console.log(bmi(height, weight))
 
 module.exports = { bmi };
