09c4c34b4dd80cb5e8b4d9a6570a0e32bbfaf803
Jean Carlos <jncorreia44@gmail.com>
2022-12-12 00:57:54 -0300

    feat: adiciona endpoint do tipo POST para adicionar um novo 'movie' ao arquivo 'movies.json'

--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expess/src/app.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expess/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { GetById, readMoviesData } = require('./utils/fsUtils');
+const { getById, readMoviesData, addMovie } = require('./utils/fsUtils');
 
 const app = express();
 
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get('/movies/:id', async (req, res) => {
   const { id } = req.params;
-  const movie = await GetById(id);
+  const movie = await getById(id);
 
   res.status(200).json(movie);
 });
@@ -16,6 +16,13 @@ app.get('/movies', async (req, res) => {
   const movie = await readMoviesData();
 
   res.status(200).json(movie);
+});
+
+app.post('/movies/add', async (req, res) => {
+  const newMovie = { ...req.body };
+  await addMovie(newMovie);
+
+  res.status(201).json(newMovie);
 });
 
 module.exports = app;
--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expess/src/utils/fsUtils.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expess/src/utils/fsUtils.js
@@ -10,7 +10,7 @@ async function readMoviesData() {
   }
 }
 
-async function GetById(id) {
+async function getById(id) {
   const moviesResponse = await fs.readFile('./src/movies.json');
   const movie = JSON.parse(moviesResponse).find(
     (where) => Number(where.id) === Number(id),
@@ -19,7 +19,21 @@ async function GetById(id) {
   if (!movie) throw new Error('Id não encontrado');
   return movie;
 }
+
+async function addMovie(newMovie) {
+  try {
+    const readMovies = await fs.readFile('./src/movies.json');
+    const movies = JSON.parse(readMovies);
+    const lastId = movies.find((_, index) => index === movies.length - 1);
+    movies.push({ id: lastId.id + 1, ...newMovie });
+
+    fs.writeFile('./src/movies.json', JSON.stringify(movies));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 module.exports = {
   readMoviesData,
-  GetById,
+  getById,
+  addMovie,
 };
