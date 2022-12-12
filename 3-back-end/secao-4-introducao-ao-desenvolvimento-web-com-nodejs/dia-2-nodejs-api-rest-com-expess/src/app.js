3137edbef40cf359dbac4f9b845e8b968ee83163
Jean Carlos <jncorreia44@gmail.com>
2022-12-12 00:35:33 -0300

    feat: adiciona endpoint do tipo GET que retorna todos os 'movies' do arquivo 'movies.json'

--- 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expess/src/app.js
+++ 3-back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expess/src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { GetById } = require('./utils/fsUtils');
+const { GetById, readMoviesData } = require('./utils/fsUtils');
 
 const app = express();
 
@@ -8,6 +8,12 @@ app.use(express.json());
 app.get('/movies/:id', async (req, res) => {
   const { id } = req.params;
   const movie = await GetById(id);
+
+  res.status(200).json(movie);
+});
+
+app.get('/movies', async (req, res) => {
+  const movie = await readMoviesData();
 
   res.status(200).json(movie);
 });
