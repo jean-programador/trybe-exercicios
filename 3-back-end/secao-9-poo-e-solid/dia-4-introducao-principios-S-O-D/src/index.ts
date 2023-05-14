abe7df2da7d2396cc1b9c0254dca45d10966c0c4
JEAN CARLOS CORREA DOS SANTOS <104799740+JeanCarlos-SC@users.noreply.github.com>
2023-05-14 07:47:56 -0300

    refactor: refatora função 'percentageGradesIntoLetters'

--- 3-back-end/secao-9-poo-e-solid/dia-4-introducao-principios-S-O-D/src/index.ts
+++ 3-back-end/secao-9-poo-e-solid/dia-4-introducao-principios-S-O-D/src/index.ts
@@ -9,24 +9,32 @@ type Student = {
   disciplines: Discipline[];
 };
 
+/* Apoio para a função `getGradeLetter` */
+const GRADE_DICT = {
+  numbers: [0.9, 0.8, 0.7, 0.6, 0.1],
+  letters: ['A', 'B', 'C', 'D', 'E'],
+};
+
+/* Função menor para remover o uso excessivo de `if{}else`s */
+const getGradeLetter = (gradeNumber: number): string => {
+  const gradeNumbers = GRADE_DICT.numbers;
+  const gradeLetters = GRADE_DICT.letters;
+  for (let i = 0; i < gradeNumbers.length; i += 1) {
+    if (gradeNumber >= gradeNumbers[i]) return gradeLetters[i];
+  }
+  return 'F';
+};
+
+/* Coletar notas */
+const getLetterGrades = (discipline: Discipline): Discipline => ({
+  ...discipline,
+  letterGrade: getGradeLetter(discipline.grade),
+});
+
 // Converter
-const percentageGradesIntoLetters = ({
-  name: studentName,
-  disciplines,
-}: Student): { name: string; disciplines: Discipline[] } => ({
-  name: studentName,
-  disciplines: disciplines.map(({ name, grade }) => {
-    let letterGrade;
-
-    if (grade >= 0.9) letterGrade = 'A';
-    else if (grade >= 0.8) letterGrade = 'B';
-    else if (grade >= 0.7) letterGrade = 'C';
-    else if (grade >= 0.6) letterGrade = 'D';
-    else if (grade >= 0.1) letterGrade = 'E';
-    else letterGrade = 'F';
-
-    return { name, grade, letterGrade };
-  }),
+const percentageGradesIntoLetters = (student: Student): Student => ({
+  ...student,
+  disciplines: student.disciplines.map(getLetterGrades),
 });
 
 // Determinar
@@ -40,9 +48,8 @@ const updateApprovalData = ({ name: studentName, disciplines }: Student): void =
   console.log(`A pessoa com nome ${studentName} foi aprovada!`);
 
   disciplines.map(({ name, letterGrade }) =>
-    console.log(`${name}: ${letterGrade}`),
-  );
-}
+    console.log(`${name}: ${letterGrade}`));
+};
 
 function setApproved(students: Array<Student>): void {
   students
